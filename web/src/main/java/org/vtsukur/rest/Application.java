a775448add06f642e284e586793c8eaa0c04abf6
vtsukur <flushdia@gmail.com>
2015-05-11 00:16:20 +0300

    Creating hotels on startup to have something to play with.

--- web/src/main/java/org/vtsukur/rest/Application.java
+++ web/src/main/java/org/vtsukur/rest/Application.java
@@ -1,13 +1,24 @@
 package org.vtsukur.rest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.vtsukur.rest.core.domain.Hotel;
+import org.vtsukur.rest.core.domain.HotelRepository;
 
 /**
  * @author volodymyr.tsukur
  */
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    CommandLineRunner init(final HotelRepository hotelRepository) {
+        return (event) -> {
+            hotelRepository.save(new Hotel("Nobilis"));
+        };
+    }
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
