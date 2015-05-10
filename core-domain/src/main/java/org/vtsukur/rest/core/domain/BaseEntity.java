af66c13dfc86556d05df608ddee55955170fb4fc
vtsukur <flushdia@gmail.com>
2015-05-10 15:32:12 +0300

    Enhancing base entity to support automatic getter and setter generation and supported JPA-backed auto generated ID values.

--- core-domain/src/main/java/org/vtsukur/rest/core/domain/BaseEntity.java
+++ core-domain/src/main/java/org/vtsukur/rest/core/domain/BaseEntity.java
@@ -1,10 +1,20 @@
 package org.vtsukur.rest.core.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * @author volodymyr.tsukur
  */
+@Getter
+@Setter
 public abstract class BaseEntity {
 
+    @Id
+    @GeneratedValue
     private Long id;
 
 }
