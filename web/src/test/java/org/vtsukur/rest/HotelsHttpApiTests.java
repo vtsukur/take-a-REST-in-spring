a210404b5f6454ddc7f1c05d8c7a28503480f078
vtsukur <flushdia@gmail.com>
2015-05-11 01:12:39 +0300

    Integration testing of retrieval of hotel via HTTP API. DRY-ing code that matches hotel data against HAL JSON.

--- web/src/test/java/org/vtsukur/rest/HotelsHttpApiTests.java
+++ web/src/test/java/org/vtsukur/rest/HotelsHttpApiTests.java
@@ -8,7 +8,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.util.JsonPathExpectationsHelper;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.vtsukur.rest.core.domain.Hotel;
@@ -60,14 +62,30 @@ public final class HotelsHttpApiTests {
                 andExpect(jsonPath("$._links.self.href", not(isEmptyOrNullString()))).
                 andExpect(jsonPath("$._links.self.templated", is(true))).
                 andExpect(jsonPath("$._embedded.hotels", hasSize(2))).
-                andExpect(jsonPath("$._embedded.hotels[0].name", is(nobilis.getName()))).
-                andExpect(jsonPath("$._embedded.hotels[0]._links.self.href", not(isEmptyOrNullString()))).
-                andExpect(jsonPath("$._embedded.hotels[1].name", is(leopolis.getName()))).
-                andExpect(jsonPath("$._embedded.hotels[1]._links.self.href", not(isEmptyOrNullString()))).
+                andExpect(hasHotel("$._embedded.hotels[0]", nobilis)).
+                andExpect(hasHotel("$._embedded.hotels[1]", leopolis)).
                 andExpect(jsonPath("$.page.size", is(20))).
                 andExpect(jsonPath("$.page.totalElements", is(2))).
                 andExpect(jsonPath("$.page.totalPages", is(1))).
                 andExpect(jsonPath("$.page.number", is(0)));
+    }
+
+    @Test
+    public void getHotel() throws Exception {
+        mockMvc.perform(get("/api/hotels/" + nobilis.getId())).
+                andExpect(status().isOk()).
+                andExpect(content().contentType(MediaTypes.HAL_JSON)).
+                andExpect(hasHotel("$", nobilis));
+    }
+
+    private static ResultMatcher hasHotel(final String prefix, final Hotel hotel) {
+        return (mvcResult) -> {
+            final String content = mvcResult.getResponse().getContentAsString();
+            new JsonPathExpectationsHelper(prefix + ".name").
+                    assertValue(content, is(hotel.getName()));
+            new JsonPathExpectationsHelper(prefix + "._links.self.href").
+                    assertValue(content, not(isEmptyOrNullString()));
+        };
     }
 
 }
