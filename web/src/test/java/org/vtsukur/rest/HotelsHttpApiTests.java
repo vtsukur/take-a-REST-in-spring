b472454d79e7d95d11290de356e50d88f969ffb8
vtsukur <flushdia@gmail.com>
2015-05-11 02:01:33 +0300

    Making matching logic for hotel's HAL JSON to be based on Spring's jsonPath and look more Hamcrest-y.

--- web/src/test/java/org/vtsukur/rest/HotelsHttpApiTests.java
+++ web/src/test/java/org/vtsukur/rest/HotelsHttpApiTests.java
@@ -1,5 +1,7 @@
 package org.vtsukur.rest;
 
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,15 +10,14 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.util.JsonPathExpectationsHelper;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.vtsukur.rest.core.domain.Hotel;
 import org.vtsukur.rest.core.domain.HotelRepository;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -62,8 +63,8 @@ public final class HotelsHttpApiTests {
                 andExpect(jsonPath("$._links.self.href", not(isEmptyOrNullString()))).
                 andExpect(jsonPath("$._links.self.templated", is(true))).
                 andExpect(jsonPath("$._embedded.hotels", hasSize(2))).
-                andExpect(hasHotel("$._embedded.hotels[0]", nobilis)).
-                andExpect(hasHotel("$._embedded.hotels[1]", leopolis)).
+                andExpect(jsonPath("$._embedded.hotels[0]", isHotel(nobilis))).
+                andExpect(jsonPath("$._embedded.hotels[1]", isHotel(leopolis))).
                 andExpect(jsonPath("$.page.size", is(20))).
                 andExpect(jsonPath("$.page.totalElements", is(2))).
                 andExpect(jsonPath("$.page.totalPages", is(1))).
@@ -75,17 +76,58 @@ public final class HotelsHttpApiTests {
         mockMvc.perform(get("/api/hotels/" + nobilis.getId())).
                 andExpect(status().isOk()).
                 andExpect(content().contentType(MediaTypes.HAL_JSON)).
-                andExpect(hasHotel("$", nobilis));
+                andExpect(jsonPath("$", isHotel(nobilis)));
     }
 
-    private static ResultMatcher hasHotel(final String prefix, final Hotel hotel) {
-        return (mvcResult) -> {
-            final String content = mvcResult.getResponse().getContentAsString();
-            new JsonPathExpectationsHelper(prefix + ".name").
-                    assertValue(content, is(hotel.getName()));
-            new JsonPathExpectationsHelper(prefix + "._links.self.href").
-                    assertValue(content, not(isEmptyOrNullString()));
-        };
+    private static MapBasedHotelRepresentationMatcher isHotel(final Hotel hotel) {
+        return new MapBasedHotelRepresentationMatcher(hotel);
+    }
+
+    /**
+     * @author volodymyr.tsukur
+     */
+    private static final class MapBasedHotelRepresentationMatcher extends TypeSafeMatcher<Map<String, ?>> {
+
+        private final Hotel reference;
+
+        private MapBasedHotelRepresentationMatcher(final Hotel reference) {
+            this.reference = reference;
+        }
+
+        @Override
+        protected boolean matchesSafely(final Map<String, ?> item) {
+            return nameMatches(item) && selfLinkPresent(item);
+        }
+
+        private boolean nameMatches(final Map<String, ?> item) {
+            return item.get("name").equals(reference.getName());
+        }
+
+        private boolean selfLinkPresent(final Map<String, ?> item) {
+            final Map<?, ?> _links = subMap(item, "_links");
+            if (_links == null) {
+                return false;
+            }
+
+            final Map<?, ?> self = subMap(_links, "self");
+            if (self == null) {
+                return false;
+            }
+
+            final String href = (String) self.get("href");
+
+            return !href.trim().isEmpty();
+        }
+
+        private static Map<?, ?> subMap(final Map<?, ?> source, final String key) {
+            return source.get(key) instanceof Map ? (Map<?, ?>) source.get(key) : null;
+        }
+
+        @Override
+        public void describeTo(final Description description) {
+            description.appendText(reference.toString());
+        }
+
     }
 
 }
