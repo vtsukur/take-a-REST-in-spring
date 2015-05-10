0483666e5caed66bb08b9dc02fa407db2e6e1e51
vtsukur <flushdia@gmail.com>
2015-05-10 14:37:40 +0300

    Authoring existing Java source files.

--- web/src/main/java/org/vtsukur/rest/Application.java
+++ web/src/main/java/org/vtsukur/rest/Application.java
@@ -3,6 +3,9 @@ package org.vtsukur.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author volodymyr.tsukur
+ */
 @SpringBootApplication
 public class Application {
 
