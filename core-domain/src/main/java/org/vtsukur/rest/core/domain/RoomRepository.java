cd51a926910216bfcc3dc5891d2374a9f419f02e
vtsukur <flushdia@gmail.com>
2015-05-11 17:42:31 +0300

    Adding repository for the room.

---
+++ core-domain/src/main/java/org/vtsukur/rest/core/domain/RoomRepository.java
@@ -0,0 +1,8 @@
+package org.vtsukur.rest.core.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public interface RoomRepository extends CrudRepository<Room, Long> {}
