a2a4fb4629aa347ec970d05a0e5fe5eaeeaa751e
vtsukur <flushdia@gmail.com>
2015-05-11 17:04:07 +0300

    Moving web-specific classes to 'web' package.

--- web/src/test/java/org/vtsukur/rest/web/ApplicationTests.java
+++ web/src/test/java/org/vtsukur/rest/web/ApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.vtsukur.rest.Application;
 
 /**
  * @author volodymyr.tsukur
