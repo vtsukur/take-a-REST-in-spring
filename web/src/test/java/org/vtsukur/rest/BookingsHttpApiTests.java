bd596452e65d04aee8bdba69842b16bc12164e90
vtsukur <flushdia@gmail.com>
2015-05-12 23:28:14 +0300

    Stub for integration tests for bookings HTTP API.

---
+++ web/src/test/java/org/vtsukur/rest/BookingsHttpApiTests.java
@@ -0,0 +1,15 @@
+//package org.vtsukur.rest;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+///**
+// * @author volodymyr.tsukur
+// */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Application.class)
+//@WebAppConfiguration
+//public class BookingsHttpApiTests {
+//}
