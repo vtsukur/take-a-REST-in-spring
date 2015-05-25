7eb01306809d5e0db350e6e7c14a917cfe7242d4
vtsukur <flushdia@gmail.com>
2015-05-25 23:31:53 +0300

    Adding retrieval of booking by id.

--- web/src/main/java/org/vtsukur/rest/BookingsCrudController.java
+++ web/src/main/java/org/vtsukur/rest/BookingsCrudController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.vtsukur.rest.core.domain.Booking;
 import org.vtsukur.rest.core.domain.BookingRepository;
 
@@ -26,6 +23,11 @@ public class BookingsCrudController {
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "25") int size) {
         return bookingRepository.findAll(new PageRequest(page, size));
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Booking getBooking(@PathVariable Long id) {
+        return bookingRepository.findOne(id);
     }
 
 }
