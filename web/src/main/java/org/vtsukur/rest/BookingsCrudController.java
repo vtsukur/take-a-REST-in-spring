e931c3f78d284909f823dfe6e6e1252af56c4a01
vtsukur <flushdia@gmail.com>
2015-05-26 21:01:10 +0300

    Booking creation.

--- web/src/main/java/org/vtsukur/rest/BookingsCrudController.java
+++ web/src/main/java/org/vtsukur/rest/BookingsCrudController.java
@@ -3,8 +3,9 @@ package org.vtsukur.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.vtsukur.rest.core.domain.Booking;
 import org.vtsukur.rest.core.domain.BookingRepository;
 
@@ -19,8 +20,11 @@ public class BookingsCrudController {
     private BookingRepository bookingRepository;
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Booking post(@RequestBody Booking booking) {
-        return bookingRepository.save(booking);
+    public ResponseEntity<Booking> post(@RequestBody Booking booking) {
+        Booking savedBooking = bookingRepository.save(booking);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(MvcUriComponentsBuilder.fromMethodName(BookingsCrudController.class, "getOne", savedBooking.getId()).build().toUri());
+        return new ResponseEntity<>(savedBooking, headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
---
+++ web/src/test/java/org/vtsukur/rest/CrudBookingsHttpApiTests.java
@@ -0,0 +1,64 @@
+package org.vtsukur.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.javamoney.moneta.Money;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.vtsukur.rest.core.domain.Booking;
+
+import javax.money.MonetaryCurrencies;
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+* @author volodymyr.tsukur
+*/
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class CrudBookingsHttpApiTests {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private ObjectMapper jsonSerializer;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void postBooking() throws Exception {
+        final String content = jsonSerializer.writeValueAsString(
+                new Booking(
+                        LocalDate.of(2015, 9, 1),
+                        LocalDate.of(2015, 9, 10),
+                        Money.of(380, MonetaryCurrencies.getCurrency("USD")))
+        );
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/crud/bookings")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().string(HttpHeaders.LOCATION, "http://localhost/crud/bookings/1"));
+    }
+
+}
