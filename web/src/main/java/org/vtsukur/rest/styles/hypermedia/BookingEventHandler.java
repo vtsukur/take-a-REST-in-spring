de6f6bd05f9bbf3f75c880b3482faf21f0a669aa
vtsukur <flushdia@gmail.com>
2015-05-30 21:56:45 +0300

    Setting up default status if it is not set.

---
+++ web/src/main/java/org/vtsukur/rest/styles/hypermedia/BookingEventHandler.java
@@ -0,0 +1,30 @@
+package org.vtsukur.rest.styles.hypermedia;
+
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+import org.vtsukur.rest.core.domain.Booking;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@Component
+@RepositoryEventHandler
+public class BookingEventHandler {
+
+    @HandleBeforeCreate
+    @HandleBeforeSave
+    public void onCreateOrSave(final Booking booking) {
+        if (booking.getStatus() == null) {
+            booking.setStatus(Booking.Status.CREATED);
+        }
+
+//        if (booking.getStatus() == Booking.Status.CREATED) {
+//            booking.setPrice(
+//                    Money.of(Period.between(booking.getCheckIn(), booking.getCheckOut()).getDays(), Currencies.USD).
+//                            multiply(booking.getPrice().getNumber()));
+//        }
+    }
+
+}
