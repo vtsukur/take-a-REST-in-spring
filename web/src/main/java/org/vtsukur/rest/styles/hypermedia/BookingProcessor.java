bdcd69cdd6724f55854ea61f9b3e593ff62cc108
vtsukur <flushdia@gmail.com>
2015-05-31 16:56:47 +0300

    Adding update and cancel links.

--- web/src/main/java/org/vtsukur/rest/styles/hypermedia/BookingProcessor.java
+++ web/src/main/java/org/vtsukur/rest/styles/hypermedia/BookingProcessor.java
@@ -1,5 +1,7 @@
 package org.vtsukur.rest.styles.hypermedia;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.stereotype.Component;
@@ -14,11 +16,16 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @Component
 public class BookingProcessor implements ResourceProcessor<Resource<Booking>> {
 
+    @Autowired
+    private EntityLinks entityLinks;
+
     @Override
     public Resource<Booking> process(Resource<Booking> resource) {
         Booking booking = resource.getContent();
         if (booking.getStatus() == Booking.Status.CREATED) {
             resource.add(linkTo(methodOn(PaymentRestController.class).pay(booking.getId(), null)).withRel("payment"));
+            resource.add(entityLinks.linkToSingleResource(booking).withRel("update"));
+            resource.add(entityLinks.linkToSingleResource(booking).withRel("cancel"));
         }
         return resource;
     }
