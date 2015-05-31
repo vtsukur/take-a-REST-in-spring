ba64db47624aa446585fb0018182b1214db14a06
vtsukur <flushdia@gmail.com>
2015-05-31 21:41:30 +0300

    Saving hotels should not be exposed.

--- web/src/main/java/org/vtsukur/rest/core/domain/HotelRepository.java
+++ web/src/main/java/org/vtsukur/rest/core/domain/HotelRepository.java
@@ -2,9 +2,20 @@ package org.vtsukur.rest.core.domain;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author volodymyr.tsukur
  */
 @RepositoryRestResource
-public interface HotelRepository extends PagingAndSortingRepository<Hotel, Long> {}
+public interface HotelRepository extends PagingAndSortingRepository<Hotel, Long> {
+
+    @RestResource(exported = false)
+    @Override
+    <S extends Hotel> S save(S entity);
+
+    @RestResource(exported = false)
+    @Override
+    <S extends Hotel> Iterable<S> save(Iterable<S> entities);
+
+}
