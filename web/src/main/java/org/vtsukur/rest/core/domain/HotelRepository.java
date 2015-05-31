24ab601a21d0a314a80a239704e437a255d08c30
vtsukur <flushdia@gmail.com>
2015-05-31 22:17:34 +0300

    Adding sample search method by city.

--- web/src/main/java/org/vtsukur/rest/core/domain/HotelRepository.java
+++ web/src/main/java/org/vtsukur/rest/core/domain/HotelRepository.java
@@ -1,6 +1,9 @@
 package org.vtsukur.rest.core.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -17,5 +20,7 @@ public interface HotelRepository extends PagingAndSortingRepository<Hotel, Long>
     @RestResource(exported = false)
     @Override
     <S extends Hotel> Iterable<S> save(Iterable<S> entities);
+
+    Page<Hotel> findByCity(@Param("city") String city, Pageable pageable);
 
 }
