6f6139b72c738117b457b639ecd59589f6aa3c3d
vtsukur <flushdia@gmail.com>
2015-05-31 17:10:55 +0300

    Registering CURIE provider.

--- web/src/main/java/org/vtsukur/rest/Application.java
+++ web/src/main/java/org/vtsukur/rest/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.hateoas.hal.CurieProvider;
+import org.springframework.hateoas.hal.DefaultCurieProvider;
 
 /**
  * @author volodymyr.tsukur
@@ -14,6 +17,11 @@ public class Application {
     @Bean
     CommandLineRunner init(final Fixture fixture) {
         return (event) -> fixture.init();
+    }
+
+    @Bean
+    CurieProvider curieProvider() {
+        return new DefaultCurieProvider("take-a-rest", new UriTemplate("http://localhost:8080/api/doc/{rel}"));
     }
 
     public static void main(final String[] args) {
